3678660f8250b7bd4c696b867831e8a294f48ec2
Riverif <rifkialfiann@gmail.com>
2024-07-30 13:17:11 +0700

    8. course edit ui page

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,5 +1,54 @@
-const CourseIdPage = ({ params }: { params: { courseId: string } }) => {
-  return <div>Course Id Page! {params.courseId}</div>;
+import { IconBadge } from "@/components/icon-badge";
+import { getCourseById } from "@/data/course";
+import { currentUser } from "@/lib/auth";
+import { LayoutDashboard } from "lucide-react";
+import { redirect } from "next/navigation";
+
+const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
+  const user = await currentUser();
+
+  if (!user) {
+    return redirect("/");
+  }
+
+  const course = await getCourseById(params.courseId);
+
+  if (!course) {
+    return redirect("/");
+  }
+
+  const requireFields = [
+    course.title,
+    course.description,
+    course.imageUrl,
+    course.price,
+    course.categoryId,
+  ];
+
+  const totalFields = requireFields.length;
+  const completedFields = requireFields.filter(Boolean).length;
+  const completionText = `(${completedFields}/${totalFields})`;
+
+  return (
+    <div className="p-6">
+      <div className="flex items-center justify-between">
+        <div className="flex flex-col gap-y-2">
+          <h1 className="text-2xl font-medium">Course Setup</h1>
+          <span className="text-sm text-slate-700">
+            Complete all fields {completionText}
+          </span>
+        </div>
+      </div>
+      <div className="mt-16 grid grid-cols-1 gap-6 md:grid-cols-2">
+        <div>
+          <div className="flex items-center gap-x-2">
+            <IconBadge icon={LayoutDashboard} />
+            <h2 className="text-xl">Customize your course</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CourseIdPage;
---
+++ data/course.ts
@@ -0,0 +1,11 @@
+import { db } from "@/lib/db";
+
+export const getCourseById = async (id: string) => {
+  try {
+    const course = await db.course.findUnique({ where: { id } });
+
+    return course;
+  } catch {
+    return null;
+  }
+};
