e932cbfbfea5a1eb41ec065fc418ead829b55f7d
Riverif <rifkialfiann@gmail.com>
2024-08-06 09:46:13 +0700

    22. course data table

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,12 +1,25 @@
-import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { redirect } from "next/navigation";
 
-const CoursesPage = () => {
+import { currentUser } from "@/lib/auth";
+
+import { Button } from "@/components/ui/button";
+import { getCourses } from "@/data/course";
+import { DataTable } from "./_components/data-table";
+import { columns } from "./_components/columns";
+
+const CoursesPage = async () => {
+  const user = await currentUser();
+
+  if (!user?.id) {
+    return redirect("/");
+  }
+
+  const courses = await getCourses(user.id);
+
   return (
     <div className="p-6">
-      <Link href="/teacher/create">
-        <Button>New Course</Button>
-      </Link>
+      <DataTable columns={columns} data={courses} />
     </div>
   );
 };
--- data/course.ts
+++ data/course.ts
@@ -1,5 +1,22 @@
 import { db } from "@/lib/db";
 
+export const getCourses = async (userId: string) => {
+  try {
+    const courses = await db.course.findMany({
+      where: {
+        userId,
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+
+    return courses;
+  } catch (error) {
+    return [];
+  }
+};
+
 export const getCourseById = async (id: string) => {
   try {
     const course = await db.course.findUnique({
