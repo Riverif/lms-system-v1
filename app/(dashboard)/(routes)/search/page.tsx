2dfd2225c505f14ba2a177aca7f536340a311e88
Riverif <rifkialfiann@gmail.com>
2024-08-06 14:07:19 +0700

    24. search query

--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -1,13 +1,20 @@
-import { getCategories } from "@/data/category";
 import { Categories } from "./_components/categories";
+
+import { getCategories } from "@/data/category";
+import { SearchInput } from "@/components/search-input";
 
 const SearchPage = async () => {
   const categories = await getCategories();
 
   return (
-    <div className="p-6">
-      <Categories items={categories} />
-    </div>
+    <>
+      <div className="px-6 pt-6 md:mb-0 md:hidden">
+        <SearchInput />
+      </div>
+      <div className="p-6">
+        <Categories items={categories} />
+      </div>
+    </>
   );
 };
 
--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -7,6 +7,7 @@ import { LogoutButton } from "./auth/logout-button";
 import Link from "next/link";
 import { IoMdExit } from "react-icons/io";
 import { LogOut } from "lucide-react";
+import { SearchInput } from "./search-input";
 
 export const NavbarRoutes = () => {
   const pathname = usePathname();
@@ -14,24 +15,32 @@ export const NavbarRoutes = () => {
 
   const isTeacherPage = pathname?.startsWith("/teacher");
   const isPlayerPage = pathname?.includes("/chapter");
+  const isSearchPage = pathname === "/search";
 
   return (
-    <div className="ml-auto flex gap-x-2">
-      {isTeacherPage || isPlayerPage ? (
-        <Link href="/">
-          <Button variant="ghost">
-            <LogOut className="mr-2 h-4 w-4" />
-            Exit
-          </Button>
-        </Link>
-      ) : (
-        <Link href="/teacher/courses">
-          <Button size="sm" variant="ghost">
-            Teacher Mode
-          </Button>
-        </Link>
+    <>
+      {isSearchPage && (
+        <div className="hidden md:block">
+          <SearchInput />
+        </div>
       )}
-      <UserButton />
-    </div>
+      <div className="ml-auto flex gap-x-2">
+        {isTeacherPage || isPlayerPage ? (
+          <Link href="/">
+            <Button variant="ghost">
+              <LogOut className="mr-2 h-4 w-4" />
+              Exit
+            </Button>
+          </Link>
+        ) : (
+          <Link href="/teacher/courses">
+            <Button size="sm" variant="ghost">
+              Teacher Mode
+            </Button>
+          </Link>
+        )}
+        <UserButton />
+      </div>
+    </>
   );
 };
